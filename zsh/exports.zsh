0fef2c640eb3e3ee000b7c6b7a5c59f8dd1ad846
Edmund Lam <2623895+edmundlam@users.noreply.github.com>
2024-03-11 21:40:39 -0400

    Update zsh installations

---
+++ zsh/exports.zsh
@@ -0,0 +1,9 @@
+# Specify my language environment, you can check your configs with `locale`
+export LANG=en_CA.UTF-8
+export LC_ALL=en_CA.UTF-8
+
+# Opt-out of Homebrew's analytics
+export HOMEBREW_NO_ANALYTICS=1
+
+# Prevent Homebrew from following redirects from HTTPS to HTTP
+export HOMEBREW_NO_INSECURE_REDIRECT=1
